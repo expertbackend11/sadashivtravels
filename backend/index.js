6098d66486e2ffed1ae4a84a2a078f1287fd9be2
expertbackend11 <agriconicindia@gmail.com>
2025-02-05 13:59:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.use(cors({
     origin: "https://sadashivtravels.com",
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
+    credentials:true
   }));
 
 
